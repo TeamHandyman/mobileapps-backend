4b9675531356950df8f7c8d6a8518c5d1a2fa63c
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-08-16 18:13:07 +0530

    Upload profile picture back end-15

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/upload',(req,res)=>{
             const newImage = new ImageModel({
                 email: req.body.email,
                 image:{
-                    data: req.file.buffer,
+                    data: req.file.buffer.toString,
                     contentType:'image/png'
                 }
             })
