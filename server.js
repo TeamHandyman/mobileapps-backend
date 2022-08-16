f5c673268ea94ee7c190986a8db33842fc13d0f2
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-08-16 16:58:06 +0530

    Upload profile picture back end-8

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ require('./config/passport')(passport)
 const Storage = multer.diskStorage({
     destination:'Propics',
     filename:(req,file,cb)=>{
-        print(file.originalname);
+        // print(file.originalname);
         cb(null,file.originalname);
     },
 });
