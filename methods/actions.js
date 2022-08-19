8f2e1a8799299e1871e0e618a6cfadcc01cf37d4
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-08-19 13:16:15 +0530

    Retrieve profile picture back end-9

--- methods/actions.js
+++ methods/actions.js
@@ -79,7 +79,7 @@ var functions = {
     },
     getPropic: async function(req,res){
         try{
-            const propic = User.findOne({
+            const propic = proPicModel.findOne({
                 email: "abc@gmail.com"
             })
             // console.log(propic+"asdsda")
