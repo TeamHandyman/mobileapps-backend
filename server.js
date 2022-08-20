e7b73b1f6e08cf5c046812d4492d6c0841d29e1e
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-08-20 20:24:34 +0530

    cloudinary-1

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const bodyParser = require('body-parser')
 const routes = require('./routes/index')
 const multer = require('multer')
 const ImageModel = require('./models/proPic')
+const cloudinary = require('cloudinary');
 
 connectDB()
+
+cloudinary.config({
+    cloud_name: 'projecthandyman',
+    api_key: '461133995855746',
+    api_secret: '-QpKX775LFGsnxH4csUfswOTQl4',
+})
 
 const app = express()
 
@@ -24,30 +31,33 @@ app.use(passport.initialize())
 require('./config/passport')(passport)
 
 //Propic storage
-const Storage = multer.diskStorage({
-    destination:'Propics',
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname);
-    },
-});
+// const Storage = multer.diskStorage({
+//     destination:'Propics',
+//     filename:(req,file,cb)=>{
+//         cb(null,file.originalname);
+//     },
+// });
 
-const upload = multer({
-    storage:Storage
-}).single('testImage')
+// const upload = multer({
+//     storage:Storage
+// }).single('testImage')
 
 app.post('/upload',(req,res)=>{
     upload(req,res,(err)=>{
         if(err){
             console.log(err)
         }else{
-            const encoded = req.body.encoded;
-            let base64 = encoded.toString('base64');
-            let propic = new Buffer(base64, 'base64');
-            const newImage = new ImageModel({
-                email: req.body.email,
-                image: propic
-            })
-            newImage.save().then(()=>res.send('successfully uploaded')).catch(err=>console.log(err))
+            cloudinary.v2.uploader.upload(req.body.path,
+                { public_id: req.body.name }, 
+                function(error, result) {console.log(result); });
+            // const encoded = req.body.encoded;
+            // let base64 = encoded.toString('base64');
+            // let propic = new Buffer(base64, 'base64');
+            // const newImage = new ImageModel({
+            //     email: req.body.email,
+            //     image: propic
+            // })
+            // newImage.save().then(()=>res.send('successfully uploaded')).catch(err=>console.log(err))
         }
     })
 })
