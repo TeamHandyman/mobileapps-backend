a34ac649a7d00d931764c3b6834ae3216dca3944
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-08-23 23:20:54 +0530

    post job customer backend

--- methods/actions.js
+++ methods/actions.js
@@ -227,7 +227,7 @@ var functions = {
     // },
     getCustomerAds: async function(req,res){
         try{
-            const ad = await proPicModel.findOne( { $text: { $search: req.body.term } } )
+            const ad = await proPicModel.findOne( { $text: { $search: "Plumber" } } )
             // console.log(propic+"asdsda")
             res.json(ad)
         }catch(err){
