e640f56d6e3a7a8ab71c763b278fa4d6e0d23f21
Deelaka Pushpakumara <2019cs125@stu.ucsc.lk>
2022-10-24 11:29:11 +0530

    qutotation model

---
+++ models/quotation.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const quoteSchema = mongoose.Schema({
+    customer:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    worker:{
+        type: String,
+        required: true
+    },
+    jobTitle : {
+        type: String,
+        required: false
+    },
+    revenueMethod :{
+        type: String,
+        required : false,
+    },
+    hourlyRate :{
+        type: Number,
+        required : false,
+    },
+    estimatedTotal :{
+        type: Number,
+        required : false,
+    },
+    description :{
+        type: String,
+        required : false,
+    },
+    imgUrl :{
+        type: String,
+        required : false,
+    },
+})
+
+module.exports = quoteModel = mongoose.model('quoteModel',quoteSchema,'quotations')
